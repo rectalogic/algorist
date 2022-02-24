d9efd6364f18930d127d4846fdba6fa7442ac3c9
Andrew Wason <rectalogic@rectalogic.com>
2022-02-24 11:28:41 -0500

    Set camera position in rules demo

--- examples/rules.py
+++ examples/rules.py
@@ -4,6 +4,9 @@ Based on http://structuresynth.sourceforge.net/learn.php
 
 import logging
 
+import bpy
+from mathutils import Matrix
+
 from algorist import Context
 
 log = logging.getLogger(__name__)
@@ -38,3 +41,23 @@ def rule1():
 
 with ctx.color(color=(0, 1, 1, 1)):
     rule1()
+
+
+bpy.context.scene.camera.matrix_world = Matrix(
+    (
+        (0.5779627561569214, -0.5464786887168884, 0.6060693860054016, 11.4134521484375),
+        (
+            0.8160631656646729,
+            0.3870314955711365,
+            -0.4292406737804413,
+            -7.15428352355957,
+        ),
+        (
+            2.9313766845007194e-06,
+            0.7426760196685791,
+            0.6696509122848511,
+            15.054122924804688,
+        ),
+        (0.0, 0.0, 0.0, 1.0),
+    )
+)
